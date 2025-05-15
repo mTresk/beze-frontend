cf5a43af33d58c8e10746df32678bde1e1c3c47d
Maxim Tresk <djtresk@gmail.com>
2025-05-15 18:09:50 +0500

    feat: improve grid layout in search page for better responsiveness
    - Updated grid styles in index.vue to use adaptive values for column and row gaps.
    - Added media query for tablet view to adjust grid columns.

--- app/pages/catalog/search/index.vue
+++ app/pages/catalog/search/index.vue
@@ -130,7 +130,13 @@ const seoDescription = 'Найденные товары в интернет-ма
     &__body {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
-        gap: rem(45) rem(25);
+
+        @include adaptive-value('column-gap', 25, 10);
+        @include adaptive-value('row-gap', 45, 20);
+
+        @media (max-width: $tablet) {
+            grid-template-columns: repeat(2, 1fr);
+        }
     }
 }
 </style>
