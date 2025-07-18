5d07be1b35828337c0ec325e12f2c409da7b1062
Maxim Tresk <djtresk@gmail.com>
2025-07-18 14:12:18 +0500

    fix: update image sizes in Hero component

--- app/components/Hero/index.vue
+++ app/components/Hero/index.vue
@@ -19,9 +19,9 @@ onUnmounted(() => {
 useSwiper(containerRef, {
     loop: true,
     speed: 1500,
-    autoplay: {
-        delay: 5000,
-    },
+    // autoplay: {
+    //     delay: 5000,
+    // },
     slidesPerView: 1,
 })
 </script>
@@ -47,7 +47,7 @@ useSwiper(containerRef, {
                     />
                     <NuxtImg
                         v-else
-                        sizes="100vw xs:100vw sm:100vw md:100vw"
+                        sizes="100vw xs:480px sm:640px md:768px"
                         class="hero__image"
                         width="768"
                         height="1152"
@@ -69,7 +69,7 @@ useSwiper(containerRef, {
                     />
                     <NuxtImg
                         v-else
-                        sizes="100vw xs:100vw sm:100vw md:100vw"
+                        sizes="100vw xs:480px sm:640px md:768px"
                         class="hero__image"
                         width="768"
                         height="1152"
@@ -91,7 +91,7 @@ useSwiper(containerRef, {
                     />
                     <NuxtImg
                         v-else
-                        sizes="100vw xs:100vw sm:100vw md:100vw"
+                        sizes="100vw xs:480px sm:640px md:768px"
                         class="hero__image"
                         width="768"
                         height="1152"
@@ -113,7 +113,7 @@ useSwiper(containerRef, {
                     />
                     <NuxtImg
                         v-else
-                        sizes="100vw xs:100vw sm:100vw md:100vw"
+                        sizes="100vw xs:480px sm:640px md:768px"
                         class="hero__image"
                         width="768"
                         height="1152"
