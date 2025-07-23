ba91309a7c6d6ccb845a17bb492aa36bf3440ecf
Maxim Tresk <djtresk@gmail.com>
2025-07-23 15:58:25 +0500

    fix: update seoDescription to include default text for product description

--- app/pages/catalog/[product].vue
+++ app/pages/catalog/[product].vue
@@ -273,7 +273,7 @@ onMounted(() => {
 })
 
 const seoTitle = computed(() => product.value?.data.seo.title ?? product.value?.data.name ?? null)
-const seoDescription = computed(() => product.value?.data.seo.description ?? product.value?.data.description ?? null)
+const seoDescription = computed(() => product.value?.data.seo.description ?? product.value?.data.description ?? `${product.value?.data.name} — Элегантная одежда на утро невесты, для дома и на каждый день`)
 const seoImage = computed(() => product.value?.data.images[0]?.retina ?? null)
 </script>
 
