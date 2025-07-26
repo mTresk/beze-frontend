e3957f7bfbc35d7ea0722f24763ebf0e7e8e0c8a
Maxim Tresk <djtresk@gmail.com>
2025-07-26 11:44:42 +0500

    refactor: remove ClientOnly wrapper from Hero component's swiper-container

--- app/components/Hero/index.vue
+++ app/components/Hero/index.vue
@@ -28,111 +28,108 @@ useSwiper(containerRef, {
 
 <template>
     <section class="hero">
-        <ClientOnly>
-            <swiper-container
-                ref="containerRef"
-                class="hero__slider"
-                :init="false"
-            >
-                <swiper-slide class="hero__slide">
-                    <NuxtImg
-                        v-if="isDesktop"
-                        sizes="md:100vw lg:100vw xl:100vw 2xl:100vw"
-                        quality="95"
-                        class="hero__image"
-                        width="1920"
-                        height="1080"
-                        src="/images/hero/1.webp"
-                        alt="Beze Studio"
-                        fetchpriority="high"
-                    />
-                    <NuxtImg
-                        v-else
-                        sizes="xs:260px sm:480px md:780px"
-                        quality="90"
-                        class="hero__image"
-                        width="768"
-                        height="1152"
-                        src="/images/hero/1-mob.webp"
-                        alt="Beze Studio"
-                        fetchpriority="high"
-                    />
-                </swiper-slide>
-                <swiper-slide class="hero__slide">
-                    <NuxtImg
-                        v-if="isDesktop"
-                        sizes="md:100vw lg:100vw xl:100vw 2xl:100vw"
-                        quality="95"
-                        class="hero__image"
-                        width="1920"
-                        height="1080"
-                        src="/images/hero/2.webp"
-                        alt="Beze Studio"
-                        fetchpriority="low"
-                    />
-                    <NuxtImg
-                        v-else
-                        sizes="xs:260px sm:480px md:780px"
-                        quality="90"
-                        class="hero__image"
-                        width="768"
-                        height="1152"
-                        src="/images/hero/2-mob.webp"
-                        alt="Beze Studio"
-                        fetchpriority="low"
-                    />
-                </swiper-slide>
-                <swiper-slide class="hero__slide">
-                    <NuxtImg
-                        v-if="isDesktop"
-                        sizes="md:100vw lg:100vw xl:100vw 2xl:100vw"
-                        quality="90"
-                        class="hero__image"
-                        width="1920"
-                        height="1080"
-                        src="/images/hero/3.webp"
-                        alt="Beze Studio"
-                        fetchpriority="low"
-                    />
-                    <NuxtImg
-                        v-else
-                        sizes="xs:260px sm:480px md:780px"
-                        quality="90"
-                        class="hero__image"
-                        width="768"
-                        height="1152"
-                        src="/images/hero/3-mob.webp"
-                        alt="Beze Studio"
-                        fetchpriority="low"
-                    />
-                </swiper-slide>
-                <swiper-slide class="hero__slide">
-                    <NuxtImg
-                        v-if="isDesktop"
-                        sizes="md:100vw lg:100vw xl:100vw 2xl:100vw"
-                        quality="95"
-                        class="hero__image"
-                        width="1920"
-                        height="1080"
-                        src="/images/hero/4.webp"
-                        alt="Beze Studio"
-                        fetchpriority="low"
-                    />
-                    <NuxtImg
-                        v-else
-                        sizes="xs:260px sm:480px md:780px"
-                        quality="90"
-                        class="hero__image"
-                        width="768"
-                        height="1152"
-                        src="/images/hero/4-mob.webp"
-                        alt="Beze Studio"
-                        fetchpriority="low"
-                    />
-                </swiper-slide>
-            </swiper-container>
-        </ClientOnly>
-
+        <swiper-container
+            ref="containerRef"
+            class="hero__slider"
+            :init="false"
+        >
+            <swiper-slide class="hero__slide">
+                <NuxtImg
+                    v-if="isDesktop"
+                    sizes="md:100vw lg:100vw xl:100vw 2xl:100vw"
+                    quality="95"
+                    class="hero__image"
+                    width="1920"
+                    height="1080"
+                    src="/images/hero/1.webp"
+                    alt="Beze Studio"
+                    fetchpriority="high"
+                />
+                <NuxtImg
+                    v-else
+                    sizes="xs:260px sm:480px md:780px"
+                    quality="90"
+                    class="hero__image"
+                    width="768"
+                    height="1152"
+                    src="/images/hero/1-mob.webp"
+                    alt="Beze Studio"
+                    fetchpriority="high"
+                />
+            </swiper-slide>
+            <swiper-slide class="hero__slide">
+                <NuxtImg
+                    v-if="isDesktop"
+                    sizes="md:100vw lg:100vw xl:100vw 2xl:100vw"
+                    quality="95"
+                    class="hero__image"
+                    width="1920"
+                    height="1080"
+                    src="/images/hero/2.webp"
+                    alt="Beze Studio"
+                    fetchpriority="low"
+                />
+                <NuxtImg
+                    v-else
+                    sizes="xs:260px sm:480px md:780px"
+                    quality="90"
+                    class="hero__image"
+                    width="768"
+                    height="1152"
+                    src="/images/hero/2-mob.webp"
+                    alt="Beze Studio"
+                    fetchpriority="low"
+                />
+            </swiper-slide>
+            <swiper-slide class="hero__slide">
+                <NuxtImg
+                    v-if="isDesktop"
+                    sizes="md:100vw lg:100vw xl:100vw 2xl:100vw"
+                    quality="90"
+                    class="hero__image"
+                    width="1920"
+                    height="1080"
+                    src="/images/hero/3.webp"
+                    alt="Beze Studio"
+                    fetchpriority="low"
+                />
+                <NuxtImg
+                    v-else
+                    sizes="xs:260px sm:480px md:780px"
+                    quality="90"
+                    class="hero__image"
+                    width="768"
+                    height="1152"
+                    src="/images/hero/3-mob.webp"
+                    alt="Beze Studio"
+                    fetchpriority="low"
+                />
+            </swiper-slide>
+            <swiper-slide class="hero__slide">
+                <NuxtImg
+                    v-if="isDesktop"
+                    sizes="md:100vw lg:100vw xl:100vw 2xl:100vw"
+                    quality="95"
+                    class="hero__image"
+                    width="1920"
+                    height="1080"
+                    src="/images/hero/4.webp"
+                    alt="Beze Studio"
+                    fetchpriority="low"
+                />
+                <NuxtImg
+                    v-else
+                    sizes="xs:260px sm:480px md:780px"
+                    quality="90"
+                    class="hero__image"
+                    width="768"
+                    height="1152"
+                    src="/images/hero/4-mob.webp"
+                    alt="Beze Studio"
+                    fetchpriority="low"
+                />
+            </swiper-slide>
+        </swiper-container>
         <div class="hero__content">
             <div class="hero__container">
                 <div class="hero__body">
