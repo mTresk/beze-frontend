ea7851b4b1114f6f13317ddf404d84d7115dcf9a
Maxim Tresk <djtresk@gmail.com>
2025-09-13 03:25:22 +0500

    fix: add dehydrate-on-visible prop to Lazy components

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,14 +2,14 @@
   <div>
     <Hero />
     <Advantages />
-    <LazyDirections />
-    <LazyBestsellers />
-    <LazyCollections />
-    <LazyAbout />
-    <LazyGallery />
-    <LazyReviews />
-    <LazyCustomers />
-    <LazyCertificate />
+    <LazyDirections dehydrate-on-visible />
+    <LazyBestsellers dehydrate-on-visible />
+    <LazyCollections dehydrate-on-visible />
+    <LazyAbout dehydrate-on-visible />
+    <LazyGallery dehydrate-on-visible />
+    <LazyReviews dehydrate-on-visible />
+    <LazyCustomers dehydrate-on-visible />
+    <LazyCertificate dehydrate-on-visible />
     <UiCursor />
   </div>
 </template>
